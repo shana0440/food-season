05af82729116d3aeed5107b304401a83c4f0970b
kwguo <shana0440@gmail.com>
2021-11-16 20:54:53 +0800

    feat: filter fruits by monthly button
    Display in season fruits only

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,54 @@
 <script>
   import fruits from "../data/fruits.json";
   import Card from "../components/Card.svelte";
+
+  let fruitsToDisplay = fruits;
+
+  const monthInPeriod = (period, month) => {
+    if (period.start <= period.end) {
+      return period.start <= month && period.end >= month;
+    } else {
+      return period.start <= month || period.end >= month;
+    }
+  };
+
+  const handleSelectMonth = (month) => () => {
+    if (month === null) {
+      fruitsToDisplay = fruits;
+    } else {
+      fruitsToDisplay = fruits.filter((fruit) => {
+        return fruit.periods.some((period) => monthInPeriod(period, month));
+      });
+    }
+  };
 </script>
 
 <h1>Food in Season</h1>
 
+<section class="mb-2">
+  <button class="btn" on:click={handleSelectMonth(null)}>全部</button>
+  <button class="btn" on:click={handleSelectMonth(1)}>1月</button>
+  <button class="btn" on:click={handleSelectMonth(2)}>2月</button>
+  <button class="btn" on:click={handleSelectMonth(3)}>3月</button>
+  <button class="btn" on:click={handleSelectMonth(4)}>4月</button>
+  <button class="btn" on:click={handleSelectMonth(5)}>5月</button>
+  <button class="btn" on:click={handleSelectMonth(6)}>6月</button>
+  <button class="btn" on:click={handleSelectMonth(7)}>7月</button>
+  <button class="btn" on:click={handleSelectMonth(8)}>8月</button>
+  <button class="btn" on:click={handleSelectMonth(9)}>9月</button>
+  <button class="btn" on:click={handleSelectMonth(10)}>10月</button>
+  <button class="btn" on:click={handleSelectMonth(11)}>11月</button>
+  <button class="btn" on:click={handleSelectMonth(12)}>12月</button>
+</section>
+
 <section class="grid grid-cols-2 gap-2">
-  {#each fruits as fruit}
+  {#each fruitsToDisplay as fruit}
     <Card item={fruit} />
   {/each}
 </section>
 
 <style lang="postcss">
+  .btn {
+    @apply w-14 py-1 px-2 font-semibold rounded-md border-2 border-gray-300;
+  }
 </style>
