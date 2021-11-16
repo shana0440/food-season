682788125303e8765b0dfdb5db20b827098b769a
kwguo <shana0440@gmail.com>
2021-11-16 22:34:15 +0800

    feat: add footer
    Describe where the data come from

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -9,6 +9,19 @@
   <slot />
 </main>
 
+<footer
+  class="border-t-2 border-dashed border-gray-200 mt-4 text-center mx-4 py-2"
+>
+  <h6 class="text-xs font-semibold">資料來源</h6>
+  <a
+    class="text-xs text-blue-500 underline"
+    target="_blank"
+    href="https://ezgo.coa.gov.tw/Fruit/TW/Season_Fruits_All"
+  >
+    農業易遊網
+  </a>
+</footer>
+
 <style global lang="postcss">
   @tailwind base;
   @tailwind components;
