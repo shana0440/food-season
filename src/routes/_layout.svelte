1ca6757d51ec21b1d206e5a0a0500bc889676aef
kwguo <shana0440@gmail.com>
2021-11-16 16:21:10 +0800

    build: init sapper project
    Add a minimum buildable project from sapper template to speed up
    development.

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<svelte:head>
+  <title>Food in Season</title>
+</svelte:head>
+
+<main>
+  <slot />
+</main>
+
+<style>
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script>
+</script>
+
+<h1>Food in Season</h1>
+
+<style>
+</style>
