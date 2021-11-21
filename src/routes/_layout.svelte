f72650db5ddd1a19471e819dd5623e0de2e206af
kwguo <shana0440@gmail.com>
2021-11-21 18:05:11 +0800

    feat: update data source at footer

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -14,7 +14,7 @@
 >
   <h6 class="text-xs font-semibold mb-2">資料來源</h6>
   <div
-    class="container grid grid-cols-2 divide-x divide-gray-200 divide-dashed"
+    class="container grid grid-cols-3 divide-x divide-gray-200 divide-dashed"
   >
     <a
       class="link"
@@ -29,6 +29,9 @@
       href="https://4b1q.coa.gov.tw/upload/files/coa_4b1q_QA/8/%E5%90%84%E6%9C%88%E4%BB%BD%E5%8F%AF%E4%BE%9B%E6%87%89%E4%B9%8B%E5%9C%8B%E7%94%A2%E8%94%AC%E8%8F%9C%E3%80%81%E6%B0%B4%E6%9E%9C%E5%93%81%E9%A0%85%E5%8F%83%E8%80%83%E8%B3%87%E6%96%99.pdf"
     >
       各月份可供應之國產蔬菜、水果品項參考資料
+    </a>
+    <a class="link" target="_blank" href="https://kmweb.coa.gov.tw/index.php">
+      農業知識入口網
     </a>
   </div>
 </footer>
